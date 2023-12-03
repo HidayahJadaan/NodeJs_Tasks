1f9fd91f2dd1abc29a91acbc121aae1fbf43148e
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-03 23:35:19 +0300

    HTTP END

--- 00NodejsCourse/app.js
+++ 00NodejsCourse/app.js
@@ -13,24 +13,3 @@ const data = require("./5-alternative");
 // console.log(data);
 
 // ================= WORKING WITH MODULES ======================
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our home page");
-  } else if (req.url === "/about") {
-    res.end("Here is our short history");
-  } else {
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `);
-  }
-  // console.log(req);
-  // res.write('Welcome to our home page');
-  // res.end();
-});
-
-// web server should keep listening to requests
-server.listen(7000);
