52fe73e4af6f0601eb426dd5e19045b050dfe86e
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-12 18:07:53 +0300

    Parsing Variables From The URL--Jonas

--- 01Node-Farm/index.js
+++ 01Node-Farm/index.js
@@ -6,20 +6,16 @@ const url = require("url");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // on each new request will log it to the console
-  // console.log(req.url);
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  // ===================================
 
-
-// ===================================
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
   const dataObj = JSON.parse(data); //array of objects
 
-// ===================================
+  // ===================================
 
-// using regular expression not "" | ' ==> because some times we have multiple items
-const replaceTemplate = (temp, product) => {
+  // using regular expression not "" | ' ==> because some times we have multiple items
+  const replaceTemplate = (temp, product) => {
     let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%PRICE%}/g, product.price);
@@ -28,43 +24,55 @@ const replaceTemplate = (temp, product) => {
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
-    
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    if (!product.organic)
+      output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
     return output;
-}
+  };
 
+  // 1. Read Template Overview(only once)
+  const tempOverview = fs.readFileSync(
+    `${__dirname}/templates/template-overview.html`,
+    "utf-8"
+  );
+  const tempProduct = fs.readFileSync(
+    `${__dirname}/templates/template-product.html`,
+    "utf-8"
+  );
+  const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+  );
 
-// 1. Read Template Overview(only once)
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-
-
-//   OVERVIEW PAGE
-  if (pathName === "/" || pathName === "/overview") {
-    
-    res.writeHead(200, {'Content-type': 'text/html'})
-    const cardsHTML = dataObj.map((card)=> replaceTemplate(tempCard, card)).join('');
-// console.log(cardsHTML);
-const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML)
+  //   OVERVIEW PAGE
+  if (pathname === "/" || pathname === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardsHTML = dataObj
+      .map((card) => replaceTemplate(tempCard, card))
+      .join("");
+    // console.log(cardsHTML);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHTML);
     res.end(output);
 
+    // PRODUCT PAGE
+  } else if (pathname === "/product") {
 
-// PRODUCT PAGE
-  } else if (pathName === "/product") {
-    res.end("THIS IS THE PRODUCT");
- 
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
+    // res.end("THIS IS THE PRODUCT");
 
-// API PAGE
-} else if (pathName === "/api") {
-   
-   
-        res.writeHead(200, {'Content-type': 'application/json'})
-        res.end(data)
-    
-// PAGE NOT FOUND
-} else {
+    // API PAGE
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    // PAGE NOT FOUND
+  } else {
     // ADDING STATUS CODE TO THE RESPONSE, writing on the header (we can use this to send some meta data about the response)
     res.writeHead(404, {
       "Content-type": "text/html",
@@ -72,7 +80,6 @@ const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML)
     }); // in Network tab in Inspect
     res.end("<h1>PAGE NOT FOUND!!!!</h1>");
   }
- 
 });
 // START LISTENING ON THE UPCOMMING REQUESTS
 server.listen(8000, "127.0.0.1", () => {
