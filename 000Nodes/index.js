8460b478d5ecf6bfe9de97ade7faf1c357fdab42
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-12 15:41:22 +0300

    Routing--> Implementing Different Actions For Different Routes- Jonas

--- 000Nodes/index.js
+++ 000Nodes/index.js
@@ -29,10 +29,31 @@ console.log(__dirname);
 // CREATING SIMPLE WEB SERVER
 
 const http = require('http');
+const { isEqualWith } = require('lodash');
+const url = require('url');
 
 const server = http.createServer((req, res)=>{
+    // on each new request will log it to the console
+    // console.log(req.url);
+
+const pathName = req.url;
+
+if(pathName=== '/' || pathName === '/overview'){
+    res.end('THIS IS THE OVERVIEW');
+}
+else if(pathName === '/product'){
+    res.end('THIS IS THE PRODUCT')
+}
+else{
+    // ADDING STATUS CODE TO THE RESPONSE, writing on the header (we can use this to send some meta data about the response)
+    res.writeHead(404, {
+'Content-type': 'text/html',
+'my-own-header': 'hello-world'
+    }); // in Network tab in Inspect
+    res.end('<h1>PAGE NOT FOUND!!!!</h1>')
+}
     // SEND BACK RESPONSE OBJECT TO THE USER ON EACH TIME A NEW REQUEST IS HAPPENED
-    res.end('HELLO FROM THE SERVER');
+    // res.end('HELLO FROM THE SERVER');
 
 });
 // START LISTENING ON THE UPCOMMING REQUESTS
