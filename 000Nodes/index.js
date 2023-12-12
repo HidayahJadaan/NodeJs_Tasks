79d219ca43463f1eb1d928cf38e58d9717a6999b
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-12 15:04:48 +0300

    Reading and writing file async- Jonas

--- 000Nodes/index.js
+++ 000Nodes/index.js
@@ -1,11 +1,26 @@
-// READING & WRITING FILES
+// READING & WRITING FILES (Sync----Blocking Way)
 fs =require('fs');
-const text= fs.readFileSync('./texts/input.txt', 'utf-8');
-const OuputText = `Output Here:   ${text}`;
-
-fs.writeFileSync('./texts/output.txt', text);
-
+// const text= fs.readFileSync('./texts/input.txt', 'utf-8');
+// const OuputText = `Output Here:   ${text}`;
+// console.log(OuputText);
+// fs.writeFileSync('./texts/output.txt', text);
+// console.log('Blocking Statements........');
 
 // PATHS
 console.log(__filename);
 console.log(__dirname);
+
+
+// READING & WRITING FILES (AsSync---- non-Blocking Way)
+
+fs.readFile('./texts/start.txt', 'utf-8', (err, data)=>{
+    // console.log(data);
+
+    fs.writeFile('./texts/final.txt', `${data}\n NEW DATA HAS BEEN WRITTEN`, 'utf-8', err =>{
+        console.log('YOUR DATA HAS BEEN WRITTEN');
+    })
+});
+
+
+
+console.log('Non-Blocking Statements........');
