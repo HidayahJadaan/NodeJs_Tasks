dcd29ef55895f58e9d95a8665db15069605d0801
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2023-12-21 16:01:11 +0300

    Auth Controller IS Done

--- 03-BookStore/routes/auth.js
+++ 03-BookStore/routes/auth.js
@@ -1,94 +1,9 @@
 const express = require("express");
+const { register, login } = require("../controllers/authorsControllers");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const asyncHandler = require("express-async-handler");
-const {
-    User,
-    validateRegisterUser,
-    validateLoginUser,
-    validateUpdateUser
-} = require('../models/User');
 
-// =================================================================
-/*
-@desc Register new user
-@method POST
-@route api/auth/register
-@access public
-*/
-router.post('/register', asyncHandler( async(req,res)=>{
-
-    // VALIDATION
-    const {error} = validateRegisterUser(req.body);
-    if(error){
-        return res.status(400).json({error:error.details[0].message});
-    }
-
-    let user = await User.findOne({email: req.body.email});
-// EMAIL CHECKING
-    if(user){
-        return res.status(400).json({error:'Email already registered'});
-    }
-// HASHING PASSWORDS
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-// NOT EXISTS --> Register it
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashedPassword,
-    });
-    // SAVE IT TO THE DATABASE
-    const userCreated = await newUser.save();
-// Don't Send Password To the User
-// const token = null; ===> without JWT
-
-// USING JWT
-const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY);
-const {password, ...other} = userCreated._doc;
-
-    res.status(201).json({...other, token});
-}));
-// =================================================================
-/*
-@desc Login user
-@method POST
-@route api/auth/login
-@access public
-*/
-router.post('/login', asyncHandler( async(req,res)=>{
-
-    // VALIDATION
-    const {error} = validateLoginUser(req.body);
-    if(error){
-        return res.status(400).json({error:error.details[0].message});
-    }
-
-    let user = await User.findOne({email: req.body.email});
-// CHECKING Credientals From User's Email
-    if(!user){
-        return res.status(400).json({error:'INVALID EMAIL OR PASSWORD'});
-    }
-// PASSWORD MATCHING
-   const isPasswordsMatch = await bcrypt.compare(req.body.password, user.password);
-
-   if(!isPasswordsMatch){
-    return res.status(400).json({error:'INVALID EMAIL OR PASSWORD'});
-   }
-
-
-// Don't Send Password To the User
-// const token = null; ===> without JWT
-
-// USING JWT
-const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET_KEY);
-const {password, ...other} = user._doc;
-
-    res.status(201).json({...other, token});
-}));
-
+router.post('/register', register);
+router.post('/login', login );
 // =================================================================
 
 module.exports = router;
